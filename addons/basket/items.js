3ce8bfb9ab1c0451cfb7d682140c57e7ce5f9bd4
Louis Echevarria <52789390+louisechevarria@users.noreply.github.com>
2024-12-13 14:29:29 -0500

    Create items.js

---
+++ addons/basket/items.js
@@ -0,0 +1,42 @@
+let basketItems = new Array();
+let itemCount = 0; 
+
+basketItems[itemCount] = {};
+basketItems[itemCount].topLabel = "Fruit"; 
+basketItems[itemCount].titleText = "Orange";
+basketItems[itemCount].bottomLabel = "$2.50";
+basketItems[itemCount].startIcon = "https://cdn.icon-icons.com/icons2/1286/PNG/512/69_85305.png";
+basketItems[itemCount].detailText = "This is the description of an orange";
+itemCount++;
+
+basketItems[itemCount] = {};
+basketItems[itemCount].topLabel = "Fruit"; 
+basketItems[itemCount].titleText = "Apple";
+basketItems[itemCount].bottomLabel = "$1.50";
+basketItems[itemCount].startIcon = "https://pics.clipartpng.com/midle/Apple_PNG_Clip_Art-1467.png";
+basketItems[itemCount].detailText = "This is the description right here, you are reading it here right now";
+itemCount++;
+
+basketItems[itemCount] = {};
+basketItems[itemCount].topLabel = "Vegetable"; 
+basketItems[itemCount].titleText = "Carrot";
+basketItems[itemCount].bottomLabel = "$0.99";
+basketItems[itemCount].startIcon = "https://cdn.pixabay.com/photo/2022/06/30/15/12/carrot-7293859_640.png";
+basketItems[itemCount].detailText = "This is the description right here, you are reading it here right now";
+itemCount++;
+
+basketItems[itemCount] = {};
+basketItems[itemCount].topLabel = "Baked Good"; 
+basketItems[itemCount].titleText = "Apple Pie";
+basketItems[itemCount].bottomLabel = "$4.50";
+basketItems[itemCount].startIcon = "https://static.vecteezy.com/system/resources/thumbnails/025/786/037/small_2x/apple-pie-isolated-illustration-ai-generative-png.png";
+basketItems[itemCount].detailText = "This is the description right here, you are reading it here right now";
+itemCount++;
+
+basketItems[itemCount] = {};
+basketItems[itemCount].topLabel = "Nuts"; 
+basketItems[itemCount].titleText = "Almond Butter";
+basketItems[itemCount].bottomLabel = "$12.50";
+basketItems[itemCount].startIcon = "https://t4.ftcdn.net/jpg/03/31/06/23/360_F_331062353_AlT5iu5Phj3RHE71DddQMaxZhPoekhlk.jpg";
+basketItems[itemCount].detailText = "This is the description right here, you are reading it here right now";
+itemCount++;  
